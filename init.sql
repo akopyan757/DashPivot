aedafd2d6dc689d26f59627cd4b4380ae7dde6f4
Albert Akopyan <akopyan757@gmail.com>
2024-10-25 01:25:06 +0400

    [Server] Created docker container for pivotdash

---
+++ init.sql
@@ -0,0 +1,2 @@
+CREATE USER akopyan_albert WITH PASSWORD 'za1avGU8';
+ALTER USER akopyan_albert WITH SUPERUSER;
--- server/src/main/kotlin/com/cheesecake/dashpivot/database/DatabaseConfig.kt
+++ server/src/main/kotlin/com/cheesecake/dashpivot/database/DatabaseConfig.kt
@@ -6,7 +6,7 @@ fun connectToDatabase() {
     Database.connect(
         url = "jdbc:postgresql://localhost:5433/pivotdash",
         driver = "org.postgresql.Driver",
-        user = "akopyanalbert",
-        password = "za1avGU8@"
+        user = "akopyan_albert",
+        password = "za1avGU8"
     )
 }
