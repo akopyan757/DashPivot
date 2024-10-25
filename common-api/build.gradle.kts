c754525807b9684a42f9102db3a7e2ce8a8c0d18
Albert Akopyan <akopyan757@gmail.com>
2024-10-25 23:45:32 +0400

    [Common-Api] Change module configuration to multiplatform library

--- common-api/build.gradle.kts
+++ common-api/build.gradle.kts
@@ -1,11 +1,18 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.kotlinMultiplatform)
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(18))
     }
+}
+
+kotlin {
+    jvm("desktop")
+    jvmToolchain(18)
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 }
---
+++ common-api/src/commonMain/kotlin/com/cheesecake/common/api/Config.kt
@@ -0,0 +1,3 @@
+package com.cheesecake.common.api
+
+const val SERVER_PORT = 8084
