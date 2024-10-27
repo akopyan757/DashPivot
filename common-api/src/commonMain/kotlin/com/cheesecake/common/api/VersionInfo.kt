82d508112b829d7e1c678e8fd6ee32fa63e68c43
Albert Akopyan <akopyan757@gmail.com>
2024-10-27 21:13:00 +0400

    [Config] Implementation of Project Versioning

--- common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
+++ common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
@@ -1,5 +1,5 @@
 package com.cheesecake.common.api
 
 object VersionInfo {
-    const val PROJECT_VERSION = "default-version" // Дефолтное значение для локальной разработки
+    const val PROJECT_VERSION = "default-version" // Default Tag
 }
