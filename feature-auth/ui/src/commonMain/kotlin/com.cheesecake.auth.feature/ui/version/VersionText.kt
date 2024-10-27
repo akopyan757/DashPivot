725606bc680a8e86425cfda0eb0b3d7fb0048659
Albert Akopyan <akopyan757@gmail.com>
2024-10-27 19:44:42 +0400

    [Common-Api] Implementation of Project Versioning

--- feature-auth/ui/src/commonMain/kotlin/com.cheesecake.auth.feature/ui/version/VersionText.kt
+++ feature-auth/ui/src/commonMain/kotlin/com.cheesecake.auth.feature/ui/version/VersionText.kt
@@ -4,13 +4,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.cheesecake.common.api.version
+import com.cheesecake.common.api.VERSION
 
 @Composable
 fun VersionText(modifier: Modifier = Modifier) {
     Text(
         modifier = modifier,
-        text = "Version: $version",
+        text = "Version: $VERSION",
         style = MaterialTheme.typography.bodyMedium
     )
 }
