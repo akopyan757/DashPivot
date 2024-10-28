58ff0a227b9a319948399a56a838ddb0d845f0c4
Albert Akopyan <akopyan757@gmail.com>
2024-10-28 17:43:52 +0400

    [Config] v0.1.45 Client

--- common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
+++ common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
@@ -1,6 +1,6 @@
 package com.cheesecake.common.api
 
 object VersionInfo {
-    const val CLIENT_VERSION = "v0.1.44"
+    const val CLIENT_VERSION = "v0.1.45"
     const val SERVER_VERSION = "v0.1.44"
 }
