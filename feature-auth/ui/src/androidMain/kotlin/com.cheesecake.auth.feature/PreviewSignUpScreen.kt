f31c5b77921d25152c6ca0c885c3800889689834
Albert Akopyan <akopyan757@gmail.com>
2024-10-28 18:21:08 +0400

    [Config] v0.1.48 Client

--- feature-auth/ui/src/androidMain/kotlin/com.cheesecake.auth.feature/PreviewSignUpScreen.kt
+++ feature-auth/ui/src/androidMain/kotlin/com.cheesecake.auth.feature/PreviewSignUpScreen.kt
@@ -2,6 +2,7 @@ package com.cheesecake.auth.feature
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.cheesecake.auth.feature.ui.SignUpScreen
 
 @Preview
 @Composable
