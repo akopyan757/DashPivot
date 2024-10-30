09954394c2b106ca443619ea8403e04860ce9d82
Albert Akopyan <akopyan757@gmail.com>
2024-10-30 05:36:00 +0400

    [Server-Auth] v0.2.3

--- common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
+++ common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
@@ -1,6 +1,6 @@
 package com.cheesecake.common.api
 
 object VersionInfo {
-    const val CLIENT_VERSION = "v0.2.2"
-    const val SERVER_VERSION = "v0.2.2"
+    const val CLIENT_VERSION = "v0.2.3"
+    const val SERVER_VERSION = "v0.2.3"
 }
