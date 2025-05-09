41e100a7ccc8b10a518b37a09b7784945e791f28
Albert Akopyan <akopyan757@gmail.com>
2025-05-09 17:28:33 +0400

    [Server-Auth] v0.2.31 Fixed code confirmation logic

--- common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
+++ common-api/src/commonMain/kotlin/com/cheesecake/common/api/VersionInfo.kt
@@ -2,5 +2,5 @@ package com.cheesecake.common.api
 
 object VersionInfo {
     const val CLIENT_VERSION = "v0.2.21"
-    const val SERVER_VERSION = "v0.2.29"
+    const val SERVER_VERSION = "v0.2.31"
 }
